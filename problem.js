c69024a145534b4ce3d935152dc89ecd6131c1c5
Roshinraj-T <roshinraj432@gmail.com>
2023-04-23 23:17:52 +0530

    done 2 problems

---
+++ problem.js
@@ -0,0 +1,27 @@
+//------------------ problem1------------------
+// numbers=[1,2,4,6,8,9,16]
+// console.log(numbers.map(e=>  e*e));
+
+// -----------------problem2-------------------
+// names=[{'name':'roshin'},{'name':'selva'},{'name':'gokul'},{'name':'jagan'}]
+// console.log(names.map(e=>e.name))
+
+// -----------------problem3-------------------
+// words=['cat','dog','cat','dog','rat','mouse']
+// count = function wordcount(){
+//     words.forEach(e=>{
+//         count.
+//     })
+// } 
+// function countWords(arr) {
+//     const map = new Map();
+//     arr.forEach(word => {
+//       map.set(word, (map.get(word) || 0) + 1);
+//     });
+//     return Object.fromEntries(map);
+//   }
+  
+//   const arr = ["apple", "banana", "cherry", "banana", "apple", "apple"];
+//   const wordCounts = countWords(arr);
+//   console.log(wordCounts); 
+  
