c190d21ec6b492a27f3f2040d4ea5d12ab562fcd
Roshinraj-T <roshinraj432@gmail.com>
2023-04-24 12:01:09 +0530

    added problem4

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Mapinjs
 Map problems in javascript
+* in map function, we can't use string as a argument so we have to change string into seperate array of value using split function
+ex: string='roshin'
+array=string.split 
+output:array={'r','o','s','h','i','n'}
--- problem.js
+++ problem.js
@@ -1,10 +1,12 @@
 //------------------ problem1------------------
 // numbers=[1,2,4,6,8,9,16]
-// console.log(numbers.map(e=>  e*e));
+// square=numbers.map(e=>  e*e)
+// console.log(square);
 
 // -----------------problem2-------------------
 // names=[{'name':'roshin'},{'name':'selva'},{'name':'gokul'},{'name':'jagan'}]
-// console.log(names.map(e=>e.name))
+// valueOfName=names.map(e=>(e.name).toUpperCase())
+// console.log(valueOfName)
 
 // -----------------problem3-------------------
 
@@ -19,3 +21,18 @@
 //     }
 //      )
 // console.log(obj)
+//====================== problrm 4=================
+// words='roshinraj';
+// obj={}
+// arrayOfLetter=words.split('')
+// arrayOfLetter.map((e)=>
+//     {      
+//         if(e in obj)
+//             return obj[e]+=1
+//         else
+//             return obj[e]=1
+//     }
+//      )
+// console.log(obj)
+
+// ========================problem 5====================
