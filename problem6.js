c60404c2b21b41b8ee31c043ff446e52cf385588
Roshinraj-T <roshinraj432@gmail.com>
2023-04-24 17:41:27 +0530

    added 6th file

---
+++ problem6.js
@@ -0,0 +1,13 @@
+// Write a function that takes an array of strings and returns a new array with only the 
+// unique strings. Use the Map object to accomplish this.
+function removeAnDuplicate (arr){
+    remove=strings.map((e)=>{
+        if(!(newString.includes(e))){
+          newString.push(e)
+        }
+      })
+      return newString
+}
+strings=['roshin','gokul','selva','jagan','roshin','gokul','singaram']
+newString=[]
+console.log(removeAnDuplicate(strings))
