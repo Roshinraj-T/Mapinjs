ea08a2446b09f25d245c86b1579ac9ae35bf9625
Roshinraj-T <roshinraj432@gmail.com>
2023-04-27 19:18:38 +0530

    modified map problems

--- problem7.js
+++ problem7.js
@@ -13,17 +13,67 @@
 //      obj[e]=  primeFactor(e)
     
 // })
-num=[1,2,3,4,5,6,7,8,9,10]
+// num=[1,2,3,4,5,6,7,8,9,10]
+// obj={}
+// num.map((e)=>{
+    // if(!(e in obj)){
+    //     obj[e]=[]
+    // }
+//     for (i=2;i<=e;i++){
+//     if (e%i==0){
+//         obj[e].push(i)
+//     }
+//     }
+// })
+// console.log(obj)
+
+num=[8]
 obj={}
 num.map((e)=>{
-    if(!(e in obj)){
+    if(typeof obj[e]=='undefined'){
         obj[e]=[]
     }
-    for (i=1;i<=e;i++){
-    if (e%i==0){
-        obj[e].push(i)
+    if(isPrime(e)){
+        obj[e].push(e)
     }
+    else{
+    obj[e]=getPrime(e)
     }
 })
+//================ getPrime function=============
+function getPrime(num){
+    a=[]
+    for(i=2;i<num;i++){
+        if((isPrime(i))&&(num%i==0)){
+            a.push(i)
+            num=num/i
+            i=1
+            if(isPrime(num)){
+                a.push(num)
+                break
+            }
+        }        
+        }
+        return a
+    }
+//=========== find prime number============
+function isPrime(number) {
+    // If the number is less than 2, it's not prime
+if (number < 2) {
+return false;
+  }
+    
+ // Check for divisors up to the square root of the number
+ for (let i = 2; i <= Math.sqrt(number); i++) {
+ if (number % i === 0) {
+// If a divisor is found, the number is not prime
+   return false;
+   }
+    }
+    
+    // If no divisors are found, the number is prime
+   return true;
+    }
+
 console.log(obj)
 
--- problem8.js
+++ problem8.js
@@ -3,12 +3,22 @@
 // an object where the keys are the age and the values are arrays of objects with that age. 
 // Use the Map object to accomplish this.
 names=[{names:"roshin",age:21},{names:"gokul",age:22},{names:"selva",age:21},{names:"jagan",age:22},{names:"singaram",age:22},{names:"vishnu",age:24}]
-obj={}
+// obj={}
+// names.map((e)=>
+// {
+//     if(!(e.age in obj)){
+//         obj[e.age]=[]
+//     }
+//     obj[e.age].push(e)
+// })
+// console.log(obj)
+obj=[]
 names.map((e)=>
 {
-    if(!(e.age in obj)){
-        obj[e.age]=[]
-    }
-    obj[e.age].push(e)
-})
-console.log(obj)
+    if(!(e.age in obj))
+    {obj[e.age]=[]}
+    obj[e.age]+=e
+    
+}
+)
+console.log(obj);
